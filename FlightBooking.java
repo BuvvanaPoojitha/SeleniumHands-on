e6fcde506c1ea6d840b066fa1f8217dd56cf40cb
BuvvanaPoojitha <96767853+BuvvanaPoojitha@users.noreply.github.com>
2022-01-04 19:13:20 +0530

    Add files via upload

---
+++ FlightBooking.java
@@ -0,0 +1,38 @@
+package seleniumhandson;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FlightBooking {
+	@Test
+	public void Booking() {
+		String URL ="https://www.tripodeal.com/";
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\91956\\eclipse-workspace\\FirstSeleniumProject\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(URL);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.id("returnDateRequiredYes")).click();
+		WebElement From = driver.findElement(By.xpath("//*[@id=\"origin\"]"));
+		From.click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement Dropdown =driver.findElement(By.xpath("//*[@id=\"eac-container-origin\"]/ul/li[3]/div"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		js.executeScript("arguments[0].click();", Dropdown);
+		
+	}
+}
